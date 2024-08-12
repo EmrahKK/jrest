48b6a4af02accfda4ba13418e71a8644b6ddaa88
EMRAH KUCUKKAYA <emrah.kucukkaya@lcwaikiki.com>
2024-08-13 00:30:01 +0300

    update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv')
 const app = express()
 const port = 8080
 const fs = require('fs-extra')
+const yaml = require('js-yaml')
 
 var morgan = require('morgan')
 app.use(morgan('combined'))
@@ -41,6 +42,59 @@ app.get('/reports', (req, res) => {
   else
     res.json({})
 })
+
+app.get('/reccomend', (req, res) => {
+  const namespace = req.query.namespace
+  const kind = req.query.kind
+  const name = req.query.name
+  const container = req.query.container
+  const format = req.query.format
+  
+  let recommended = {}
+
+  if (namespace in jsonObject) {
+    for (var i = 0; i < jsonObject[namespace].length; i++) {
+      if (jsonObject[namespace][i]["object"]["name"] == name && jsonObject[namespace][i]["object"]["kind"] == kind) {
+          // Transform the JSON using reduce
+          const transformedJson = {
+            "resources": {
+              "requests": transformSection(jsonObject[namespace][i]["recommended"]["requests"]),
+              "limits": transformSection(jsonObject[namespace][i]["recommended"]["limits"])
+            }              
+          }
+
+          if (format == "yaml") {
+            // Convert the transformed JSON to YAML
+            const yamlOutput = yaml.dump(transformedJson)
+            res.send(yamlOutput)
+          } else
+            res.json(transformedJson)
+
+      } else
+      res.json({}) 
+    }
+  } else
+    res.json({})
+})
+
+// Function to transform each section
+const transformSection = (section) => {
+    return Object.keys(section).reduce((acc, key) => {
+        let value = section[key].value;
+
+        if (key === 'memory' && value !== null) {
+            value = (value / 1048576) + 'Mi'; // Convert to MB and add 'Mi' suffix
+        } else if (key === 'cpu' && value !== null) {
+            value = value;
+        }
+
+        if (value !== null) {
+            acc[key] = value;
+        }
+
+        return acc;
+    }, {});
+};
 
 app.listen(port, () => {
   console.log(`Jrest app listening at http://localhost:${port}`)
